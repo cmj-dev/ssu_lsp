19f80a22ecd796e1f92faa006e82c96844a003c0
cmj-dev <louis9982.dev@gmail.com>
2022-03-07 16:47:03 +0900

    b1 last

--- b1/open/open.c
+++ b1/open/open.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int	main(void)
 {
+	
+	char	*fname = "ssu_test.txt";
 	int		fd;
-	char	*file_name;
 
-	file_name = "ssu_test.txt";
 	if ((fd = open(file_name, O_RDONLY)) < 0)
 	{
-		fprintf(stderr, "open error for %s\n", file_name);
+		fprintf(stderr, "open error for %s\n", fname);
 		exit(1);
 	}
 	else
-		printf("Success!\nFilename : %s\nDescriptor : %d\n", file_name, fd);
+		printf("Success!\nFilename : %s\nDescriptor : %d\n", fname, fd);
+
 	exit(0);
 }
